163ca03e04bdcc2e16cfc74ca4a8564b38d7541b
williamsnieves <williamsnieves@gmail.com>
2014-11-12 22:28:07 -0430

    profile completed web

--- app/gruntfile.js
+++ app/gruntfile.js
@@ -1,5 +1,19 @@
 module.exports = function(grunt){
 	grunt.initConfig({
+		watch: {
+		  css: {
+		    files: '**/*.css',
+		    options: {
+		      livereload: true
+		    }
+		  },
+		  html: {
+		  	files: '**/*.html',
+		    options: {
+		      livereload: true
+		    }
+		  }
+		},
 		shell : {
 			options: {
             	stderr: false
@@ -7,9 +21,26 @@ module.exports = function(grunt){
         	target: {
             	command: 'stylus -c -w -o css/ stylus/webstyle.styl'
         	}
+		},
+		nodemon: {
+	      dev: {
+	        script: 'newserver3.js',
+	        delayTime: 1
+	      }
+	    },
+	    concurrent: {
+		    target: {
+		        tasks: ['nodemon', 'watch', 'shell'],
+		        options: {
+		            logConcurrentOutput: true,
+		        }
+		    }
 		}
 	});
 	grunt.loadNpmTasks('grunt-shell');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-nodemon');
+	grunt.loadNpmTasks('grunt-concurrent');
 
-	grunt.registerTask('default', ['shell']);
+	grunt.registerTask('default', ['concurrent', 'nodemon','watch','shell']);
 }
