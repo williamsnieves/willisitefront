d9302a73b2742cacb3080ff43ff43260b38af973
williamsnieves <williamsnieves@gmail.com>
2014-12-23 21:51:22 -0430

    finished template web

--- app/js/app.js
+++ app/js/app.js
@@ -1,17 +1,40 @@
-$(function(){
-	$(document).foundation();
-
-	var $container = $('#container-wall');
+function setMasonryGrid(){
+	var $container = $('#container-wall') || "";
 	// layout Masonry again after all images have loaded
-	$container.imagesLoaded(function() {
-	  $container.masonry({
-	  	 columnWidth: 1,
-	  	 itemSelector : '.box'
-	  });
-	});
+	if(!$container){
+		return;
+	}else{
 
+		$container.imagesLoaded(function() {
+		  $container.masonry({
+		  	 columnWidth: 1,
+		  	 itemSelector : '.box'
+		  });
+		});
+	}
+}
+
+function displayMenuMobile(){
 	$("#main-menu-display").on("click", function(e){
 		e.preventDefault();
 		$(".main-menu").slideToggle();
 	})
+}
+
+function displayMoreProfile(){
+	$(".expand-more-profile").on("click", function(e){
+		e.preventDefault();
+		$(".web-more-profile").slideToggle();
+	})
+}
+ 
+
+$(function(){
+	$(document).foundation();
+
+	setMasonryGrid();
+
+	displayMenuMobile();
+
+	displayMoreProfile();
 })
